4adcda1a72800ce527bb83eb3aaef86370f95d17
prettyant <prettyant@qq.com>
2023-06-02 16:04:34 +0800

    -

---
+++ loanAndroid/base/build/generated/ap_generated_sources/debug/out/com/prettyant/base/BR.java
@@ -0,0 +1,5 @@
+package com.prettyant.base;
+
+public class BR {
+  public static int _all = 0;
+}
---
+++ loanAndroid/base/build/generated/data_binding_trigger/debug/com/prettyant/base/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.prettyant.base;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
